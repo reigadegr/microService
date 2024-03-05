6e12573d878c39c0dd9372f807402beb63217c13
reigadegr <2722688642@qq.com>
2024-03-05 10:10:59 +0800

    add controller

---
+++ product_service/src/main/java/com/huike/product/controller/ProductController.java
@@ -0,0 +1,21 @@
+package com.huike.product.controller;
+
+import com.huike.product.entity.Product;
+import com.huike.product.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/product")
+public class ProductController {
+    @Autowired
+    private ProductService productService;
+    @RequestMapping(value="/${id}",method = RequestMethod.GET)
+    public Product findById(@PathVariable Long id){
+        Product product = productService.findById(id);
+        return product;
+    }
+}
