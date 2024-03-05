98081e5673fbd05fa03995395586345131343ae7
reigadegr <2722688642@qq.com>
2024-03-05 14:14:25 +0800

    添加监听器

--- product_service/src/main/java/com/huike/product/ProductApplication.kt
+++ product_service/src/main/java/com/huike/product/ProductApplication.kt
@@ -1,7 +1,9 @@
 package com.huike.product
 
+import com.huike.product.startlintener.SpringStartListener
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.context.ConfigurableApplicationContext
 import org.springframework.boot.autoconfigure.domain.EntityScan
 
 @SpringBootApplication
@@ -9,9 +11,13 @@ import org.springframework.boot.autoconfigure.domain.EntityScan
 open class ProductApplication {
     companion object {
         @JvmStatic
-        fun main(args: Array<String>) {
+ fun main(args: Array<String>) {
             println("Hello world!")
-            SpringApplication.run(ProductApplication::class.java, *args)
+            val configurableApplicationContext: ConfigurableApplicationContext =
+                SpringApplication.run(ProductApplication::class.java, *args)
+
+            // 向Spring上下文中添加监听器
+            configurableApplicationContext.addApplicationListener(SpringStartListener())
         }
     }
 }
