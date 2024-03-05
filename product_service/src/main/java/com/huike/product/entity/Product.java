9b6f7cc484744dae5317a3db6d4e475ddd26da78
reigadegr <2722688642@qq.com>
2024-03-05 09:41:37 +0800

    use kotlin profile

--- product_service/src/main/java/com/huike/product/entity/Product.java
+++ product_service/src/main/java/com/huike/product/entity/Product.java
@@ -6,18 +6,17 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import java.math.BigDecimal;
+
 @Data
 @Entity
-@Table(name="tb_product")
+@Table(name = "tb_product")
 public class Product {
     @Id
     private Long id;
     private String productName;
-
     private Integer status;
     private BigDecimal price;
     private String productDesc;
     private String caption;
     private Integer inventory;
-
 }
